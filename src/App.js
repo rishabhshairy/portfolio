fc4b9d2454b21aac294130cce9aca2df362513b7
rishabhshairy <rishusinha8@gmail.com>
2021-05-03 23:25:50 +0530

    Minor Fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import './App.css';
-import Hero from './components/Hero';
-import Sidebar from './components/Sidebar';
+import "./App.css";
+import About from "./components/About";
+import Hero from "./components/Hero";
+import Sidebar from "./components/Sidebar";
+import "./css/style.css";
 
 function App() {
   return (
     <div>
-      <div className="container-wrap"><Sidebar></Sidebar></div>
-      <div className="container-fluid"><Hero></Hero></div>
+      <div className="container-wrap">
+        <Sidebar></Sidebar>
+      </div>
+      <div className="container-fluid">
+        <Hero></Hero>
+        <div id="main">
+          <About></About>
+        </div>
+      </div>
     </div>
   );
 }
