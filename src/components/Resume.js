dbc685f337cbed1a0f116fc766fead11908622d7
rishabhshairy <rishusinha8@gmail.com>
2021-05-09 00:21:16 +0530

    Added Resume Details
    Removed Code Duplication

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,118 +1,112 @@
 import React from "react";
 import "../css/style.css";
 export default function Resume() {
+  // const summaryDesc = ``;
+  const info = {
+    name: `Rishabh Shairy`,
+    emailId: `rishabhshairy29@gmail.com`,
+    city: `Bengaluru, Karnataka`,
+    desc: `Innovative and deadline-driven Software Engineer 1.5+ years
+    of experience designing and developing user-centered
+    softwares. Experienced in mapping clients’ business requirements and translating these requirements into functional
+    specifications, services, implementations and custom designing solutions`,
+  };
+
+  const educationInfo = [
+    {
+      id: 1,
+      collegeName: `St. Thomas' College Of Engineering and Technology`,
+      year: `August 2015 - June 2019`,
+      degree: `B.Tech in Computer Science and Engineering`,
+      marks: `8.81 GPA`,
+    },
+    {
+      id: 2,
+      collegeName: `Jawahar Vidya Mandir, Shyamali`,
+      year: `June 2012 - May 2014`,
+      degree: `Senior Secondary Education / Class XII`,
+      marks: `84 %`,
+    },
+    {
+      id: 3,
+      collegeName: `Jawahar Vidya Mandir, Shyamali`,
+      year: `March 2011 - May 2012`,
+      degree: `Secondary Education / Class X`,
+      marks: `9.2 GPA`,
+    },
+  ];
+
+  const profExp = [
+    {
+      role: `System Engineer`,
+      year: `April 2020 - Present`,
+      company: `Infosys Ltd`,
+      location: `Bengaluru, Karnataka`,
+    },
+    {
+      role: `System Engineer Trainee`,
+      year: `November 2019 - March 2020`,
+      company: `Infosys Ltd`,
+      location: `Mysore, Karnataka`,
+    },
+    {
+      role: `Full Stack Developer Intern`,
+      year: `March 2019 - October 2019`,
+      company: `Thinking Stack Inc`,
+      location: `Bengaluru, Karnataka`,
+    },
+  ];
+
   return (
-    <div id="resume" className="resume" style={{padding:"60px 0"}}>
+    <div id="resume" className="resume" style={{ padding: "60px 0" }}>
       <div className="container" data-aos="fade-up">
         <div className="section-title">
           <h2>Resume</h2>
-          <p>
-            Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex
-            aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos
-            quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia
-            fugiat sit in iste officiis commodi quidem hic quas.
-          </p>
         </div>
 
         <div className="row">
           <div className="col-lg-6">
             <h3 className="resume-title">Sumary</h3>
             <div className="resume-item pb-0">
-              <h4>Brandon Johnson</h4>
+              <h4>{info.name}</h4>
               <p>
-                <em>
-                  Innovative and deadline-driven Graphic Designer with 3+ years
-                  of experience designing and developing user-centered
-                  digital/print marketing material from initial concept to
-                  final, polished deliverable.
-                </em>
+                <em>{info.desc}</em>
               </p>
               <ul>
-                <li>Portland par 127,Orlando, FL</li>
-                <li>(123) 456-7891</li>
-                <li>alice.barkley@example.com</li>
+                <li>{info.city}</li>
+                <li>{info.emailId}</li>
               </ul>
             </div>
 
             <h3 className="resume-title">Education</h3>
-            <div className="resume-item">
-              <h4>Master of Fine Arts &amp; Graphic Design</h4>
-              <h5>2015 - 2016</h5>
-              <p>
-                <em>Rochester Institute of Technology, Rochester, NY</em>
-              </p>
-              <p>
-                Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam
-                iusto autem sit. Ea vero voluptatum qui ut dignissimos deleniti
-                nerada porti sand markend
-              </p>
-            </div>
-            <div className="resume-item">
-              <h4>Bachelor of Fine Arts &amp; Graphic Design</h4>
-              <h5>2010 - 2014</h5>
-              <p>
-                <em>Rochester Institute of Technology, Rochester, NY</em>
-              </p>
-              <p>
-                Quia nobis sequi est occaecati aut. Repudiandae et iusto quae
-                reiciendis et quis Eius vel ratione eius unde vitae rerum
-                voluptates asperiores voluptatem Earum molestiae consequatur
-                neque etlon sader mart dila
-              </p>
-            </div>
+            {educationInfo.map((edItem) => {
+              return (
+                <div className="resume-item" key={edItem.id}>
+                  <h4>{edItem.degree}</h4>
+                  <h5>{edItem.year}</h5>
+                  <p>
+                    <em>{edItem.collegeName}</em>
+                  </p>
+                  <p>{edItem.marks}</p>
+                </div>
+              );
+            })}
           </div>
           <div className="col-lg-6">
             <h3 className="resume-title">Professional Experience</h3>
-            <div className="resume-item">
-              <h4>Senior graphic design specialist</h4>
-              <h5>2019 - Present</h5>
-              <p>
-                <em>Experion, New York, NY </em>
-              </p>
-              <ul>
-                <li>
-                  Lead in the design, development, and implementation of the
-                  graphic, layout, and production communication materials
-                </li>
-                <li>
-                  Delegate tasks to the 7 members of the design team and provide
-                  counsel on all aspects of the project.{" "}
-                </li>
-                <li>
-                  Supervise the assessment of all graphic materials in order to
-                  ensure quality and accuracy of the design
-                </li>
-                <li>
-                  Oversee the efficient use of production project budgets
-                  ranging from $2,000 - $25,000
-                </li>
-              </ul>
-            </div>
-            <div className="resume-item">
-              <h4>Graphic design specialist</h4>
-              <h5>2017 - 2018</h5>
-              <p>
-                <em>Stepping Stone Advertising, New York, NY</em>
-              </p>
-              <ul>
-                <li>
-                  Developed numerous marketing programs (logos,
-                  brochures,infographics, presentations, and advertisements).
-                </li>
-                <li>
-                  Managed up to 5 projects or tasks at a given time while under
-                  pressure
-                </li>
-                <li>
-                  Recommended and consulted with clients on the most appropriate
-                  graphic design
-                </li>
-                <li>
-                  Created 4+ design presentations and proposals a month for
-                  clients and account managers
-                </li>
-              </ul>
-            </div>
+            {profExp.map((exp) => {
+              return (
+                <div className="resume-item">
+                  <h4>{exp.role}</h4>
+                  <h5>{exp.year}</h5>
+                  <p style={{ fontWeight: "bold" }}>{exp.company}</p>
+                  <p>
+                    <em>{exp.location}</em>
+                  </p>
+                  <ul></ul>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
