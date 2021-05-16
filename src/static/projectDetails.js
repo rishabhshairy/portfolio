073c781e6c73ccf49c576dcf0d4e0093818550e4
rishabhshairy <rishusinha8@gmail.com>
2021-05-16 20:55:22 +0530

    Converted Project Details To JS files

---
+++ src/static/projectDetails.js
@@ -0,0 +1,57 @@
+export const projectData = [
+  {
+    id: 1,
+    projectName: "Apple Online Store -- ( Publishing Systems )",
+    year: "April 2020 - Present",
+    role: "Publishing System Engineer",
+    company: "Infosys Ltd",
+    image: "eCommProject",
+    details: {
+      description: "",
+      responsibilities: [],
+      tech: [],
+    },
+  },
+  {
+    id: 2,
+    projectName: "Wanderlust",
+    year: "January 2020 - March 2020",
+    role: "System Engineer Trainee",
+    company: "Infosys Ltd",
+    image: "wanderlust",
+    details: {
+      description:
+        "Web Application for tour and travel purpose, where user can login, book, review, search for the tour packages around the world",
+      responsibilities: [
+        "Developing backend services, web controllers using Java, Spring-boot",
+        "Designing database as per the requirement.",
+        "Coordinating & collaborating with Front-end teams to implement features for the platform.",
+        "Testing the features using Unit Tests.",
+      ],
+      tech: [
+        {
+          Backend: "Java, Spring-Boot",
+        },
+        {
+          FrontEnd: "React JS, HTML, CSS, Bootstrap, Javascript",
+        },
+        {
+          Database: "My-SQL",
+        },
+      ],
+    },
+  },
+  {
+    id: 3,
+    projectName: "AI Platform",
+    year: "March 2019 - October 2019",
+    role: "Full Stack Developer Intern",
+    company: "Thinking Stack Inc",
+    image: "fullStackProj",
+    details: {
+      description: "",
+      responsibilities: [],
+      tech: [],
+    },
+  },
+];
