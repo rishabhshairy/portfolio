b56b613891168ba6e5a866226fae6af5796edcfa
rishabhshairy <rishusinha8@gmail.com>
2021-07-07 21:28:21 +0530

    Added More info on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,41 +2,48 @@ import React from "react";
 import "../css/style.css";
 export default function Footer() {
   return (
-      <footer id="footer">
-        <div className="container container-small">
-          <h3>Rishabh Shairy</h3>
-          <a href="mailto:rishabhshairy29@gmail.com">
-            rishabhshairy29@gmail.com
+    <footer id="footer">
+      <div className="container container-small">
+        <h3>Rishabh Shairy</h3>
+        <a href="mailto:rishabhshairy29@gmail.com">rishabhshairy29@gmail.com</a>
+        <div className="social-links">
+          <a href="https://github.com/rishabhshairy" className="github">
+            <i className="bx bxl-github"></i>
           </a>
-          <div className="social-links">
-            <a href="https://github.com/rishabhshairy" className="github">
-              <i className="bx bxl-github"></i>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/rishabhshairy/"
-              className="linkedin"
-            >
-              <i className="bx bxl-linkedin"></i>
-            </a>
-            <p className="justify-content-center"> 
-              Hosted on GitHub Pages with{" "}
-              <span
-                className="bx bxs-heart"
-                style={{ color: "red", fontSize:"22px",marginTop:"4px" }}
-              ></span>
-            </p>
-          </div>
-          <div className="copyright">
-            &copy; Copyright{" "}
-            <strong>
-              <span>MyResume</span>
-            </strong>
-            . All Rights Reserved
-          </div>
-          <div className="credits">
-            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-          </div>
+          <a
+            href="https://www.linkedin.com/in/rishabhshairy/"
+            className="linkedin"
+          >
+            <i className="bx bxl-linkedin"></i>
+          </a>
         </div>
-      </footer>
+        <div>
+          <p className="justify-content-center">
+            Hosted on GitHub Pages with{" "}
+            <span
+              className="bx bxs-heart"
+              style={{ color: "red", fontSize: "22px", marginTop: "4px" }}
+            ></span>
+          </p>
+          <p className="justify-content-center">
+            Automated Deployment Using Travis CI{" "}
+            <span
+              className="bx bxs-rocket"
+              style={{ color: "cadetblue", fontSize: "20px", marginTop: "4px" }}
+            ></span>
+          </p>
+        </div>
+        <div className="copyright">
+          &copy; Copyright{" "}
+          <strong>
+            <span>MyResume</span>
+          </strong>
+          . All Rights Reserved
+        </div>
+        <div className="credits">
+          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        </div>
+      </div>
+    </footer>
   );
 }
